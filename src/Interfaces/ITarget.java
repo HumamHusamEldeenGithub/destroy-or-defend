9677ac28ff243d5799275733da6b3e24cb0a6b96
LAPTOP-RIMF369T\Moeyn <moeynaise@gmail.com>
2020-11-14 15:27:00 +0200

    ITarget and towers implementation

--- src/Interfaces/ITarget.java
+++ src/Interfaces/ITarget.java
@@ -1,8 +1,8 @@
 package Interfaces;
 
 import Units.Unit;
-
+import java.util.List;
 public interface ITarget {
     boolean CanTarget(Unit unit);
-
+    List<Object> CheckRange();
 }
--- src/Units/Towers.java
+++ src/Units/Towers.java
@@ -1,12 +1,36 @@
 package Units;
 
+import Arena.Arena;
 import Interfaces.ITarget;
+import Utilities.Position;
 
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Towers extends Unit implements ITarget{
+
+    @Override
+    public List<Object> CheckRange() {
+        List<Object> Enemies = new ArrayList<>();
+        for(int i=this.position.x - this.range ; i<= this.position.x + this.range ; i++){
+            for(int j=this.position.y - this.range ; j<= this.position.y + this.range ; j++){
+                if(i==position.x && j==position.y)
+                    continue;
+                Position curPos = new Position(i,j);
+                Unit curUnit = Arena.CheckPos(curPos);
+                if(curUnit!=null && CanTarget(curUnit)){
+                    Enemies.add(curUnit);
+                }
+            }
+        }
+
+
+        return Enemies;
+    }
+
     @Override
     public boolean CanTarget(Unit unit) {
-        return false;
+        return true;
     }
 }

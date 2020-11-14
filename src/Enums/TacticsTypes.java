2c15c8a40cd070d6dfb9693d5fed87db05b38266
Humam Husam  Eldeen <64328929+Humam-H-E1@users.noreply.github.com>
2020-11-14 17:09:13 +0200

    Adding TacticsTypes Enum .

---
+++ src/Enums/TacticsTypes.java
@@ -0,0 +1,5 @@
+package Enums;
+
+public enum TacticsTypes {
+    LowestHealth,HighestDamage , Randomly
+}
--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,6 +1,7 @@
 package Game;
 
 import Arena.Arena;
+import Enums.TacticsTypes;
 import Interfaces.IAttack;
 import Units.Unit;
 
@@ -10,17 +11,21 @@ public class Game {
 
     class Team {
         ArrayList<Unit> Units = new ArrayList<Unit>();
+        Tactics TeamTactics = new Tactics(TacticsTypes.Randomly);
         class Player{}
+        class Tactics  {
+            TacticsTypes TeamTactic = null ;
+            public Tactics(TacticsTypes tacticsTypes)
+            {
+                TeamTactic = tacticsTypes ;
+            }
+        }
         public void AddUnit(Unit unit){
             Units.add(unit);
         }
         public void DestroyUnit(Unit unit){
             Units.remove(unit);
         }
-       /* class Tactics implements IAttack {
-            void Attack(Unit unit) {
 
-            }
-        }*/
     }
 }

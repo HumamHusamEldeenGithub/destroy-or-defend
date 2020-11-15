9a181dd31d93b174c9b2099232d07450c64abc5d
LAPTOP-RIMF369T\Moeyn <moeynaise@gmail.com>
2020-11-15 12:45:14 +0200

    Javafx lib add

--- src/Utilities/main.java
+++ src/Utilities/main.java
@@ -4,8 +4,23 @@ import Arena.Arena;
 import Enums.UnitType;
 import Units.Forces;
 import Units.Unit;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class main {
+
+public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
     public static void main(String[] args){
         Unit soldier = new Forces(UnitType.RifleSoldier) ;
         Arena.Deploy(soldier,new Position(5,5)) ;
